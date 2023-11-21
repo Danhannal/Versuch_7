c8b4fd80412a128cfd885d5bb9077498c1e1c6a1
Kieran <kieranh@web.de>
2023-11-21 11:14:33 +0000

    add JAVADOC!!

---
+++ src/versuch_7/BanditData.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package versuch_7;
+
+/**
+ *
+ * @author Kieran
+ */
+
+
+
+
+
+
+
+//add javadoc
+
+
+public class BanditData 
+{
+  private final int value;
+  private final int ID;
+  public BanditData(int inputValue, int inputID)
+  {
+    value = inputValue;
+    ID = inputID;
+  }
+  
+  public int getValue()
+  {
+    return value;
+  }
+  public int getID()
+  {
+    return ID;
+  }
+}
